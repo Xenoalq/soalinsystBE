139fd8e4325bbf226fd563d5552e47d01ee7ec41
Xenoalq <skynikeshd11@gmail.com>
2025-11-20 13:25:39 +0700

    Add InsystBE project

--- InsystBE/routes/api.php
+++ InsystBE/routes/api.php
@@ -4,6 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\TransactionController;
 
-Route::apiResource('products', ProductController::class);
+// API Crud
+Route::get('/products', [ProductController::class, 'index']);      
+Route::post('/products', [ProductController::class, 'store']);     
+Route::get('/products/{id}', [ProductController::class, 'show']);  
+Route::put('/products/{id}', [ProductController::class, 'update']); 
+Route::delete('/products/{id}', [ProductController::class, 'destroy']); 
 
+// Transaction
 Route::post('/transactions', [TransactionController::class, 'store']);
